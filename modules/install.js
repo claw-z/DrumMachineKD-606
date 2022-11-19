d68e56761a4735fed7d44554bd537a3a697ede29
claw-z <you@example.com>
2022-11-19 15:57:56 +0100

    changed btn in install.js and started adjusting css for volume fader integration

--- modules/install.js
+++ modules/install.js
@@ -12,7 +12,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
   // Update UI to notify the user they can add to home screen
-  addBtn.style.display = 'block';
+  addBtn.style.display = 'inline-block';
 
   addBtn.addEventListener('click', (e) => {
     // hide our user interface that shows our A2HS button
