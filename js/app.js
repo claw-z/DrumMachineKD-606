e56197bb3e4c7f2375ebc4381e29afbd0d6f3f6c
claw-z <you@example.com>
2023-01-04 14:33:22 +0100

    code formatted with prettier in app.js

--- js/app.js
+++ js/app.js
@@ -1,66 +1,75 @@
-import { addMouseOver, clearPattern, clearVoice, copyFlag, instantPatternToggle, kitSelector, modeSwitch, removeMouseOver, toggleSound, toggleStep } from "../modules/buttons.js";
+import {
+  addMouseOver,
+  clearPattern,
+  clearVoice,
+  copyFlag,
+  instantPatternToggle,
+  kitSelector,
+  modeSwitch,
+  removeMouseOver,
+  toggleSound,
+  toggleStep,
+} from "../modules/buttons.js";
 import { showLoadArea } from "../modules/db_load.js";
 import { showSaveArea } from "../modules/db_save.js";
 import { addInstallButton } from "../modules/install.js";
 import { click, el, group } from "../modules/lib.js";
-import { initValue, initTime, loopToggle, spaceBarPlay } from "../modules/main.js";
+import {
+  initValue,
+  initTime,
+  loopToggle,
+  spaceBarPlay,
+} from "../modules/main.js";
 import { serviceWorkerAktiv } from "../modules/service-aktiv.js";
-
 
 // Init values
 initValue();
 initTime();
 
-
 // META BUTTONS
 // Assigns showSaveArea to button '#save'
-click('#save',showSaveArea);
+click("#save", showSaveArea);
 // Assigns showLoadArea to button '#load'
-click('#load',showLoadArea);
+click("#load", showLoadArea);
 // Adds install functionality
 // serviceWorkerAktiv();
 // addInstallButton();
 
-
 // CONTROL AREA
 // Note: volume is read by sound.js, BPM is read by main.js
 // Assign loopToggle to 'Play' button and space bar
-click('#play-stop',loopToggle);
+click("#play-stop", loopToggle);
 spaceBarPlay();
-
 
 // MODE SWITCH & INST PTN
 // Toggle between step and pattern modes
-click('#mode-switch', modeSwitch);
-click('#instant-checkbox', instantPatternToggle);
-
+click("#mode-switch", modeSwitch);
+click("#instant-checkbox", instantPatternToggle);
 
 // PATTERN-KIT AREA
 // Assign clearPattern to button '#ptn-clear'
-click('#ptn-clear',clearPattern);
+click("#ptn-clear", clearPattern);
 // Assign copyFlag to button '#copy'
-click('#copy',copyFlag);
+click("#copy", copyFlag);
 // Assign kitSelector to button '#kit-selector'
-click('#kit-selector',kitSelector);
+click("#kit-selector", kitSelector);
 // Assign clearVoice to button '#voice-clear'
-click('#voice-clear',clearVoice);
-
+click("#voice-clear", clearVoice);
 
 // SOUND BUTTONS
 // Assign toggleSound to sound buttons
-group('#sounds button').forEach((button)=>{
-  button.addEventListener('click',toggleSound)
+group("#sounds button").forEach((button) => {
+  button.addEventListener("click", toggleSound);
 });
-
 
 // STEP BUTTONS
 // Assign toggleStep to step buttons
-group('#sequencer button').forEach((button)=>{
-  button.addEventListener('click',toggleStep)
+group("#sequencer button").forEach((button) => {
+  button.addEventListener("click", toggleStep);
 });
 // Mousedown functionality for step buttons inside of '#sequencer' div
-el('#sequencer').addEventListener('mousedown', (e) => {
+el("#sequencer").addEventListener("mousedown", (e) => {
   addMouseOver();
-  e.preventDefault();}
-);
-el('#sequencer').addEventListener('mouseup', removeMouseOver);
+  e.preventDefault();
+});
+el("#sequencer").addEventListener("mouseup", removeMouseOver);
